19cec07eb6506a7684b1c619d2d90ce27531eb22
Samira77-ss <samiraseyfii0709@gmail.com>
2023-04-16 10:52:09 +0330

    Added the GitHub rep URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Samira Seyfi and is{" "}
+        <a
+          href="https://github.com/Samira77-ss/react-weather-app7"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
