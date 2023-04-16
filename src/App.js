4a70ed176c0270c793860b30c91fd4f0d2f3db0e
Samira77-ss <samiraseyfii0709@gmail.com>
2023-04-16 23:51:32 +0330

    Added Bootstrsp

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Samira Seyfi and is{" "}
-        <a
-          href="https://github.com/Samira77-ss/react-weather-app7"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Samira Seyfi and is{" "}
+          <a
+            href="https://github.com/Samira77-ss/react-weather-app7"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
